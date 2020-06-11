80b78a866cb176dda3f3f6f95afc6f6d088be80d
lwj18033585194 <40883465+lwj18033585194@users.noreply.github.com>
2020-06-11 08:41:30 +0800

    Update Home.vue

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -7,7 +7,7 @@
 
 <script>
     import HomeHeader from './components/Header'
-    import HomeSwiper from './components/swiper'
+    import HomeSwiper from './components/Swiper'
     export default {
         name:'Home',
         components:{HomeHeader,HomeSwiper}
